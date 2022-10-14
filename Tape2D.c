cdf32eb559dab52d83abda131bab0f8450bbee9c
Heathcorp <heath@manningf.com>
2022-10-15 02:47:52 +1100

    Implement 2D tape struct

---
+++ Tape2D.c
@@ -0,0 +1,40 @@
+#include "Tape2D.h"
+#include "List2D.h"
+
+#include <stdlib.h>
+
+struct Tape2D T2D_init() {
+	struct Tape2D tape;
+	tape.row = 0;
+	tape.col = 0;
+	tape.quad0 = L2D_init();
+	tape.quad1 = L2D_init();
+	tape.quad2 = L2D_init();
+	tape.quad3 = L2D_init();
+	tape.head = L2D_access(&tape.quad0, 0, 0);
+	return tape;
+}
+
+int T2D_free(struct Tape2D* tape) {
+	L2D_free(&tape->quad0);
+	L2D_free(&tape->quad1);
+	L2D_free(&tape->quad2);
+	L2D_free(&tape->quad3);
+}
+
+
+int T2D_move(struct Tape2D* tape, int row, int col) {
+	if(row >= 0 && col >= 0) {
+		// quadrant 0
+		tape->head = L2D_access(&tape, row, col);
+	} else if(row >= 0 && col < 0) {
+		// quadrant 1
+		tape->head = L2D_access(&tape, row, -col - 1);
+	} else if(row < 0 && col < 0) {
+		// quadrant 2
+		tape->head = L2D_access(&tape, -row - 1, -col - 1);
+	} else if(row < 0 && col >= 0) {
+		// quadrant 3
+		tape->head = L2D_access(&tape, -row - 1, col);
+	}
+}
---
+++ Tape2D.h
@@ -0,0 +1,21 @@
+#ifndef TAPE_2D_H
+#define TAPE_2D_H
+
+#include "List2D.h"
+
+struct Tape2D {
+	struct List2D quad0;
+	struct List2D quad1;
+	struct List2D quad2;
+	struct List2D quad3;
+
+	int row, col;
+	char* head;
+};
+
+struct Tape2D T2D_init();
+int T2D_free(struct Tape2D* tape);
+
+int T2D_move(struct Tape2D* tape, int row, int col);
+
+#endif
